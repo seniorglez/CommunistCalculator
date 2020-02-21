73bd911a1c476c289db18cc15134c4718590d7d4
Diego Domínguez <d.dominguez.glez1111@gmail.com>
2020-02-21 16:00:08 +0100

    working on a method that allows me to calculate stuff between brackets

--- app/src/main/java/com/example/calculator/model/Calculator.java
+++ app/src/main/java/com/example/calculator/model/Calculator.java
@@ -1,5 +1,9 @@
 package com.example.calculator.model;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     private String patten = "(?=[\\^*+\\-\\/])|(?<=[\\^*+\\-\\/])";
@@ -12,6 +16,21 @@ public class Calculator {
     public static Calculator getCalculatorInstance(){
         if(cal==null) cal= new Calculator();
         return cal;
+    }
+
+    private String[] findBrackets(String str){
+        String regex = "\\((.*?)\\)";//if you try to put a bracket inside a brackets group if will fail
+        ArrayList<String>s= new ArrayList<>();
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            System.out.println("Full match: " + matcher.group(0));
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                s.add(matcher.group(i));//maybe i can just replace the group here calculating the result
+            }
+        }
+        return (String[]) s.toArray();
     }
 
     public double calculate(String operation) {
